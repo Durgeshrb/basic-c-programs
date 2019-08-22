209231cb80bc4b069467b0dddb77221c6545af90
Durgesh_rb <54394168+Durgeshrb@users.noreply.github.com>
2019-08-22 22:40:21 +0530

    Add files via upload

---
+++ range_addition.c
@@ -0,0 +1,17 @@
+//addition of given range
+#include<stdio.h>
+
+void main()
+{
+	int a,b,sum=0;
+	i=a;
+
+	printf("enter range for addition of them");
+	scanf("%d%d",&a,&b);
+
+	while (i>=a && i<=b)
+	{
+		sum=sum+i;
+	}
+	printf("addition of given range is : ",sum);
+}
