703a39beaa246990c7e6099aebb41940dc3a6bb7
Durgesh_rb <54394168+Durgeshrb@users.noreply.github.com>
2019-08-22 22:39:24 +0530

    Add files via upload

---
+++ print_1to100.c
@@ -0,0 +1,15 @@
+//print odd nobetween 1 to 100
+#include<stdio.h>
+
+void main()
+{
+	int i;
+	printf("odd numbers between 1 to 100 are:\n");
+	for(i=1;i<=100;i++)
+	{
+		if(i%2==1)
+		{
+			printf("i\n");
+		}
+	}
+}
