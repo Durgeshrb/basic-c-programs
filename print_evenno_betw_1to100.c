3ead2c4f0ecad68ad774604ae2cf792177897e72
Durgesh_rb <54394168+Durgeshrb@users.noreply.github.com>
2019-08-22 22:39:51 +0530

    Add files via upload

---
+++ print_evenno_betw_1to100.c
@@ -0,0 +1,15 @@
+//print even no between 1 to 100
+#include<stdio.h>
+
+void main()
+{
+	int i;
+	printf("even numbers between 1 to 100 are:\n");
+	for(i=1;i<=100;i++)
+	{
+		if(i%2==0)
+		{
+			printf("i\n");
+		}
+	}
+}
