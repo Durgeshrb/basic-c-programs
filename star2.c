251a4043643852ce0935702ebf14b9f8f132b301
Durgesh_rb <54394168+Durgeshrb@users.noreply.github.com>
2019-08-24 21:43:34 +0530

    Add files via upload
    print star like 
    * * * * *
    * * * * 
    * * * 
    * *
    *

---
+++ star2.c
@@ -0,0 +1,22 @@
+/*print * * * * *
+	* * * * 
+	* * * 
+	* * 
+	* 
+*/	
+#include<stdio.h>
+
+int main()
+{
+	int i,j;
+	for(i=0;i<5;i++)
+	{
+		for(j=5;j>i;j--)
+		{
+			printf("*\t");
+		}	
+		printf("\n");
+	}
+	return 0;
+}
+
