74cd9f609e87f1be9ef19035692eda298762fd5d
Durgesh_rb <54394168+Durgeshrb@users.noreply.github.com>
2019-08-24 22:03:28 +0530

    Create star3.c

---
+++ star3.c
@@ -0,0 +1,22 @@
+/*print the following	 
+	* 
+	* *
+	* * *
+	* * * * 
+	* * * * *
+*/	
+#include<stdio.h>
+
+int main()
+{
+	int i,j;
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<=i;j++)
+		{
+			printf("*\t");
+		}	
+		printf("\n");
+	}
+	return 0;
+}
